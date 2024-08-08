9f604144c0bdfd6b22fc10d892483bd2ea01c461
replicapra <replicapra@outlook.com>
2024-08-08 18:24:35 +0200

    add disko

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
