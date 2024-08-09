c7d35f3639f465bb706699d9dd2c749f08a61922
replicapra <replicapra@outlook.com>
2024-08-09 07:43:04 +0000

    add openssh

---
+++ modules/nixos/services/openssh/default.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.services.openssh;
+in
+{
+  options.${namespace}.services.openssh = {
+    enable = mkEnableOption "openssh";
+  };
+
+  config = mkIfEnbled cfg {
+    services = {
+      openssh = enabled // {
+        settings = {
+          X11Forwarding = false;
+          UseDns = true;
+          PermitRootLogin = "no";
+          PasswordAuthentication = false;
+        };
+        extraConfig = ''
+          PubkeyAuthentication yes
+          PermitEmptyPasswords no
+          ChallengeResponseAuthentication no
+          PermitUserEnvironment no
+          AllowAgentForwarding no
+          AllowTcpForwarding no
+          PermitTunnel no
+          ClientAliveInterval 300
+        '';
+      };
+    };
+  };
+}
